edfb48eb01ab2425071fc228554db05b6cce3c05
ShailRT <shailesh18h@gmail.com>
2023-09-27 20:57:14 +0530

    base create

--- city_spot/models.py
+++ city_spot/models.py
@@ -4,7 +4,7 @@ import uuid
 type_choices =(
     ('Shelter', 'shelter'),
     ('Shopping', 'shopping'),
-    ('Waterfall', 'waterfall')
+    ('Waterfall', 'waterfall'),
     ('Trek', 'trek'),
     ('Temple', 'temple'),
 )
@@ -17,7 +17,7 @@ class Spot(models.Model):
     spot_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     slug = models.SlugField()
-    spot_type = models.CharField(max_length=120, choices=type_choices)
+    spot_type = models.CharField(max_length=120, choices=type_choices, default='N/A')
     main_image = models.ImageField(upload_to="spot_image/")
     gallery = models.ManyToManyField(Gallery, null=True, blank=True)
     description = models.TextField()
