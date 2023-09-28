65f37978609764aeb1be2c52ab75bd54410e02bd
ShailRT <shailesh18h@gmail.com>
2023-09-28 09:20:23 +0530

    9am update

--- city_spot/models.py
+++ city_spot/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 type_choices =(
     ('Shelter', 'shelter'),
     ('Shopping', 'shopping'),
     ('Waterfall', 'waterfall'),
+    ('Lake', 'lake'),
     ('Trek', 'trek'),
     ('Temple', 'temple'),
 )
@@ -22,7 +24,7 @@ class Spot(models.Model):
     slug = models.SlugField()
     spot_type = models.CharField(max_length=120, choices=type_choices, default='N/A')
     main_image = models.ImageField(upload_to="spot_image/")
-    gallery = models.ManyToManyField(Gallery, null=True, blank=True)
+    gallery = models.ManyToManyField(Gallery)
     description = models.TextField()
     city = models.CharField(max_length=20, blank=True, null=True)
     state = models.CharField(max_length=20, blank=True, null=True)
@@ -30,5 +32,19 @@ class Spot(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Trip(models.Model):
+    trip_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    note = models.TextField(blank=True, null=True)
+    spots = models.ManyToManyField(Spot)
+    date_created = models.DateField(auto_now_add=True, null=True, blank=True)
+    
+class Profile(models.Model):
+    profile_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trips = models.ManyToManyField(Trip)
+    
+    
 
 
